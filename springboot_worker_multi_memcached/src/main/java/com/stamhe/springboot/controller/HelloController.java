9b2f3a68961e644e979f8b9172a901779059db22
Stam He <stamhe@163.com>
2019-04-17 15:08:18 +0800

    新增cache操作实例

--- springboot_worker_multi_memcached/src/main/java/com/stamhe/springboot/controller/HelloController.java
+++ springboot_worker_multi_memcached/src/main/java/com/stamhe/springboot/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.stamhe.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +24,15 @@ public class HelloController {
 		String key = "k1";
 		// 过期时间单位: s
 		cacheObj.set(key, 60, "v1");
+		long start_time = System.nanoTime();
 		String value = cacheObj.get(key).toString();
+		long end_time = System.nanoTime();
+		System.out.println("start_time = " + start_time + " end_time = " + end_time);
 		
 		return value;
 	}
 	
-	@RequestMapping("obj")
+	@RequestMapping("/obj")
 	public UserModel objAction()
 	{
 		MemcachedClient cacheObj = memcachedInit.getClient();
@@ -42,5 +48,36 @@ public class HelloController {
 		UserModel objModel = (UserModel)cacheObj.get(key);
 		
 		return objModel;
+	}
+	
+
+	@RequestMapping("/objlist")
+	public List<UserModel> objlistAction()
+	{
+		MemcachedClient cacheObj = memcachedInit.getClient();
+		
+		List<UserModel> list = new ArrayList<>();
+		
+		UserModel userModel1 = new UserModel();
+		userModel1.setId(1001L);
+		userModel1.setUsername("hequan-1");
+		userModel1.setPassword("123456-1");
+		
+
+		UserModel userModel2 = new UserModel();
+		userModel2.setId(1002L);
+		userModel2.setUsername("hequan-2");
+		userModel2.setPassword("123456-2");
+		
+		list.add(userModel1);
+		list.add(userModel2);
+		
+		String key = "k2-objlist";
+		// 过期时间单位: s
+		cacheObj.set(key, 60, list);
+		
+		List<UserModel> list2 = (List<UserModel>)cacheObj.get(key);
+		
+		return list2;
 	}
 }
