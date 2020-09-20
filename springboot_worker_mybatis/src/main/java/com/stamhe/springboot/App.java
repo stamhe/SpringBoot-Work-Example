b49c0e8078e144279a09263175d86b3b0dea9da6
Stam He <stamhe@163.com>
2020-09-20 11:02:33 +0800

    启用 lombok

--- springboot_worker_mybatis/src/main/java/com/stamhe/springboot/App.java
+++ springboot_worker_mybatis/src/main/java/com/stamhe/springboot/App.java
@@ -3,10 +3,8 @@ package com.stamhe.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.stamhe.springboot"})
 @MapperScan("com.stamhe.springboot.mapper")
 public class App 
 {
--- springboot_worker_mybatis/src/main/java/com/stamhe/springboot/model/ArticleModel.java
+++ springboot_worker_mybatis/src/main/java/com/stamhe/springboot/model/ArticleModel.java
@@ -1,21 +1,10 @@
 package com.stamhe.springboot.model;
 
+import lombok.Data;
+
 import java.util.Date;
 
-/*
-CREATE TABLE `t_article_201908_001` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `summary` varchar(1024) NOT NULL DEFAULT '',
-  `status` int(11) NOT NULL DEFAULT '0',
-  `type` int(11) NOT NULL,
-  `user_id` bigint(20) NOT NULL DEFAULT '0',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `public_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
- */
+@Data
 public class ArticleModel {
 	private Long id;
     private String title;
@@ -27,67 +16,4 @@ public class ArticleModel {
     private Date createTime;
     private Date publicTime;
     private Date updateTime;
-    
-    
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getSummary() {
-		return summary;
-	}
-	public void setSummary(String summary) {
-		this.summary = summary;
-	}
-	public Integer getStatus() {
-		return status;
-	}
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-	public Integer getType() {
-		return type;
-	}
-	public void setType(Integer type) {
-		this.type = type;
-	}
-	public Long getUserId() {
-		return userId;
-	}
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-	public Date getCreateTime() {
-		return createTime;
-	}
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-	public Date getPublicTime() {
-		return publicTime;
-	}
-	public void setPublicTime(Date publicTime) {
-		this.publicTime = publicTime;
-	}
-	public Date getUpdateTime() {
-		return updateTime;
-	}
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
-	
-	@Override
-	public String toString() {
-		return "ArticleModel [id=" + id + ", title=" + title + ", summary=" + summary + ", status=" + status + ", type="
-				+ type + ", userId=" + userId + ", createTime=" + createTime + ", publicTime=" + publicTime
-				+ ", updateTime=" + updateTime + "]";
-	}
 }
